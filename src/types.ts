37699e001730541ea5b09ae203d01e48538ed784
Irvin Torres <irvinrx1996@hotmail.com>
2025-10-31 01:28:25 -0600

    chore: Initial commit

---
+++ src/types.ts
@@ -0,0 +1,7 @@
+// src/types.ts
+
+/** Defines the valid evaluation strategies */
+export type Strategy = 'first' | 'last' | 'random';
+
+/** Defines the valid button style types */
+export type ButtonType = 'filled' | 'outlined';
